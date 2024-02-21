3fbc4c7b9db5c131a77141c2350f4059e2a2f58b
Jubair Ibn Khaled <160339037+Pro-jubair@users.noreply.github.com>
2024-02-21 11:37:24 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function jog (x,y){
+
+    var result = x + y;
+    document.write("jogfol = " + result + "<br>");
+}
+
+function biyog (x,y){
+
+    var result = x - y;
+    document.write("biyogfol = " + result + "<br>");
+}
+
+
+function gun (x,y){
+
+    var result = x * y;
+    document.write("gunfol = " + result + "<br>");
+}
+
+
+
+jog(10,20);
+biyog(10,50);
+gun(10,50);
